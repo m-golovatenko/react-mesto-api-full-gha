b1f17ce1fce5559e6f0b35f4908e790c70cd2476
Maria Golovatenko <pikch4y@yandex.com>
2023-08-08 13:58:42 +0300

    hotfix: change port

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/users');
 const { validationLogin, validationCreateUser } = require('./middlewares/validation');
 
-const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 4001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
